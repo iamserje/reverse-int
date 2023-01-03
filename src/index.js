8494da89a0df874b17a910c2096d5eb5fd16bfbc
iamserje <iamserje@gmail.com>
2023-01-03 12:29:21 +0300

    done: reverse-int is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  const stringN = String(Math.abs(n));
+  let res = [];
+  for (i in stringN) {
+   res.push(stringN[i]);
+  }
+   let final = '';
+   for (i in res.reverse()) {
+      final += res[i];
+   }
+   return +final;
 }
